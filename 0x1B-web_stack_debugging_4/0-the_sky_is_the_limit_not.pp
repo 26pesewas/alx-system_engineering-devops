fb65455ce3faf3678ec30c9c17742b495de1476c
root <naaakuokor@gmail.com>
2022-11-10 22:37:29 +0000

    debug

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,11 @@
-# Sky is the limit, let's bring that limit higher
-exec { 'Skylimit':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx; service nginx restart',
-  provider => shell
+# Increase the ULIMIT of the default file
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
+
+# Restart Nginx
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,7 +1,14 @@
-# Change the OS configuration so that it is possible to login with
-# the holberton user and open a file without any error message.
-exec { 'Userlimit':
-  command => 'sed -i "s/5/50/" /etc/security/limits.conf; \
-  sed -i "s/4/40/" /etc/security/limits.conf;',
-  provider => shell
+# Enable the user holberton to login and open files without error.
+
+# Increase hard file limit for Holberton user.
+exec {'hard':
+path     => ['/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/'],
+command  => "sudo sed -i 's/holberton hard nofile 5/holberton hard nofile 65536/g' /etc/security/limits.conf; /sbin/sysctl -p",
+provider => 'shell',
+}
+# Increase soft file limit for Holberton user.
+exec {'soft':
+path     => ['/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/'],
+command  => "sudo sed -i 's/holberton soft nofile 4/holberton soft nofile 65536/g' /etc/security/limits.conf; /sbin/sysctl -p",
+provider => 'shell',
 }
